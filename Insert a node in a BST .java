9971014e34bbce3e33287e00efac6976103402ca
Vikash Kumar <88263645+Vikash620404@users.noreply.github.com>
2022-07-13 00:25:32 +0530

    Update Insert a node in a BST .java

--- Insert a node in a BST .java
+++ Insert a node in a BST .java
@@ -19,3 +19,41 @@ class Solution {
 
 //O(Height of the BST).
 //O(Height of the BST).
+
+
+
+
+
+class Solution {
+    // Function to insert a node in a BST.
+    Node insert(Node root, int Key) {
+       if(root==null) return new Node(Key);
+       Node curr=root;
+       while(curr!=null){
+          if(curr.data==Key) break;
+           if(curr.data>Key){
+               if(curr.left==null){
+                   curr.left=new Node(Key);
+                   break;
+               }else{
+                   curr=curr.left;
+               }
+           }
+           else{
+               if(curr.right==null){
+                   curr.right=new Node(Key);
+                   break;
+               }else{
+                   curr=curr.right;
+               }
+           }
+       }
+       return root;
+    }
+}
+
+
+
+
+//O(Height of the BST).
+//O(1).
